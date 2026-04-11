d90f50a24e90014b0ed0ae5ce7a1148983080167
pavan <pavankumarbalagani4u@gmail.com>
2026-04-11 09:27:23 +0530

    added

---
+++ script.js
@@ -0,0 +1,71 @@
+//console.log("hello");
+//console.log(-2);
+//var n=10;
+//console.log(n);
+//console.log(typeof n);
+// var n1=890;
+// var n2=780;
+// if(n1>n2){
+//     console.log(1)
+// }
+// else{
+//     console.log(0)
+// }
+// if(10 == '10')//checks content only
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// if(10 === '10') //checks value and content
+// {
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+// console.log(2*3);//mul
+// console.log(2**3)//exponent
+
+// var r=30
+// var b=true;
+// for(var i=2;i<=r;i++)
+// {
+//     b=true;
+//     for(var j=2;j<=i/2;j++)
+//     {
+//         if(i%j==0)
+//         {
+//             b=false;
+//             break;
+//         }
+//     }
+//     if(b) console.log(i);
+// }
+// var a=10;
+// var a=20;
+// console.log(a);
+
+// const c=30;
+// console.log(c);
+
+// let d=90;
+//  d=2;
+// console.log(d**3)
+
+let userData=[10,"pavan",false,9,null,'bye']
+//console.log(userData);
+for(let i=0;i<userData.length;i++)
+{
+    //console.log(userData[i])
+    console.log(typeof userData[i])
+}
+var ar=[12,"pavan",false,89]
+ar[0]=90;
+console.log(ar)
+
+
+const ar1=[12,"mahesh",80,true]
+ar1[1]="sai";
+console.log(ar1);
