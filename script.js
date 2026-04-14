fe431c9eb8df61b0e9e82d1baa8ba9829c3bda90
pavan <pavankumarbalagani4u@gmail.com>
2026-04-14 08:56:57 +0530

    functions added

--- script.js
+++ script.js
@@ -94,18 +94,88 @@
 //     name: "pavan",
 //     myData:[2,"pavan",true]
 // }
-const user=[
-    {
-        id : 1,
-        name: "pavan",
-        city: "hyd"     
-    },
-    {
-        id :2,
-        name: "kaushik",
-        city: "krn"
+// const user=[
+//     {
+//         id : 1,
+//         name: "pavan",
+//         city: "hyd"     
+//     },
+//     {
+//         id :2,
+//         name: "kaushik",
+//         city: "krn"
+//     }
+// ]
+// console.log(user[0])
+// console.log(user[0].name)
+
+// function MyFun(){
+//     console.log(-1)
+//     return -2
+// }
+//MyFun()
+//console.log(MyFun())
+
+// function add(n1,n2){
+//     return n1+n2
+// }
+
+// //console.log(add(2,3))
+
+// const myFun =function (n1,n2){//annonymus function
+//     return n1+n2
+// }
+// //console.log(myFun(8,9))
+// function sub(n1,n2){
+//      return n1-n2
+// }
+// function mul(n1,n2){
+//     return n1*n2
+// }
+// function div(n1,n2)
+// {
+//     return n1/n2
+// }
+
+// function doMath(n1,n2,key){
+//     console.log(`numbers are ${n1} and ${n2}`)
+//     switch (key) {
+//         case 1:
+//             console.log("Addition of"+n1+"&"+n2+" is "+add(n1,n2))
+//             break;
+//         case 2:
+//             console.log("subration of "+n1+" & "+n2+" is "+sub(n1,n2))
+//             break;
+//         case 3:
+//             console.log("multiplication of "+n1+" & "+n2+" is "+mul(n1,n2))
+//             break;
+//         case 4:
+//             console.log("division of"+n1+" & "+n2+" is "+div(n1,n2))
+//             break;
+//         default:
+//             break;
+//     }
+// }
+function doMath(n1,n2,key){
+    switch (key) {
+        case 1:
+            console.log(`addition of two numbers ${n1} and ${n2} is ${n1+n2}`)
+            break;
+        case 2:
+            console.log(`subtraction of two numbers  ${n1} and ${n2} is ${n1-n2}`);
+            break;
+        case 3:
+            console.log(`multiplication of two numbers  ${n1} and ${n2} is ${n1*n2}`);
+            break;
+        case 4:
+            console.log(`division of two numbers  ${n1} and ${n2} is ${n1/n2}`);
+            break;
+    
+        default:
+            break;
     }
-]
-console.log(user[0])
-console.log(user[0].name)
+}
+doMath(20,2,4)
+
+
 
